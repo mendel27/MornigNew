a089d8894ccaa465ed848447353e2fdcb5575633
mendel27 <107189908+mendel27@users.noreply.github.com>
2022-07-15 11:22:26 +0200

    mongodb

--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ var options = {
     useNewUrlParser: true,
 }
 
-mongoose.connect('mongodb+srv://david:hgm4lRQq8QM1p3P8@cluster0-9xbpy.mongodb.net/morningnews?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://mendel27:Callof2327@cluster0.h443f.mongodb.net/morningnews?retryWrites=true&w=majority',
     options,
     function(err){
         console.log(err);
